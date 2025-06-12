eda406f6f19de50c978a83d1a557669f1303524d
Arslan Yousaf <devtechofficial90@gmail.com>
2025-06-12 18:04:10 +0500

    Enhance WelcomePage with page change functionality and add DotsIndicator for navigation

--- lib/pages/welcome/view.dart
+++ lib/pages/welcome/view.dart
@@ -4,6 +4,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:get/get.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 
 class WelcomePage extends GetView<WelcomeController> {
   const WelcomePage({super.key});
@@ -11,7 +12,8 @@ class WelcomePage extends GetView<WelcomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
+        body: Obx(
+      () => SizedBox(
         width: 360.w,
         height: 780.w,
         child: Stack(
@@ -20,7 +22,9 @@ class WelcomePage extends GetView<WelcomeController> {
             PageView(
               scrollDirection: Axis.horizontal,
               reverse: false,
-              onPageChanged: (index) {},
+              onPageChanged: (index) {
+                controller.changePage(index);
+              },
               controller: PageController(
                   initialPage: 0, keepPage: true, viewportFraction: 1),
               pageSnapping: true,
@@ -41,7 +45,7 @@ class WelcomePage extends GetView<WelcomeController> {
                   height: double.infinity,
                   decoration: const BoxDecoration(
                     image: DecorationImage(
-                      image: AssetImage('assets/images/banner1.png'),
+                      image: AssetImage('assets/images/banner2.png'),
                       fit: BoxFit.fill,
                     ),
                   ),
@@ -51,16 +55,59 @@ class WelcomePage extends GetView<WelcomeController> {
                   height: double.infinity,
                   decoration: const BoxDecoration(
                     image: DecorationImage(
-                      image: AssetImage('assets/images/banner1.png'),
+                      image: AssetImage('assets/images/banner3.png'),
                       fit: BoxFit.fill,
                     ),
                   ),
+                  child: Stack(
+                    alignment: Alignment.bottomCenter,
+                    children: [
+                      Positioned(
+                        bottom: 90,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.white),
+                            foregroundColor:
+                                MaterialStateProperty.all(Colors.black),
+                            shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            side: MaterialStateProperty.all(
+                              const BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          onPressed: () => null,
+                          child: Text("Login"),
+                        ),
+                      ),
+                    ],
+                  ),
                 )
               ],
-            )
+            ),
+            Positioned(
+                bottom: 70,
+                child: DotsIndicator(
+                  position: controller.state.index.value.toDouble(),
+                  dotsCount: 3,
+                  reversed: false,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  decorator: DotsDecorator(
+                    size: const Size.square(9.0),
+                    activeSize: const Size(18.0, 9.0),
+                    activeShape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                )),
           ],
         ),
       ),
-    );
+    ));
   }
 }
